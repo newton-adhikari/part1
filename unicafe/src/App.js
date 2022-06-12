c670053b7831c94529829d05172aa89c99e95ec3
NewtonAdhikari <newtonadk@gmail.com>
2022-06-12 17:30:54 +0545

    unicafe step1 updated

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodHandler = () => {
+    setGood(good + 1);
+  }
+
+  const neutralHandler = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const badHandler = () => {
+    setBad(bad +1);
+  }
+
   return (
-    <p>unicafe</p>
-  );
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={goodHandler}>good</button>
+      <button onClick={neutralHandler}>neutral</button>
+      <button onClick={badHandler}>bad</button>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
